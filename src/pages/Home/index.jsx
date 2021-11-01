d9be56e14e8af312736cdc07f77f6f0f1e740e94
Jana Malecka <jm.jmalecka@gmail.com>
2021-11-01 13:51:07 +0100

    text on homepage added

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,7 +6,20 @@ const Home = () => {
   return (
     <>
       <div className="container">
-        <h2>Jana Malecká</h2>
+        <h1>Hi there, I'm Jana.</h1>
+        <h2>
+          Cause I haven't <span className="italic">meta</span> a mentor
+        </h2>
+        <h3>who would take me under their React wing</h3>
+        <h4>so far...</h4>
+        <h5>it looks like how it looks!</h5>
+      </div>
+      <div className="container">
+        <p>
+          My website is a work in progress if you haven't noticed yet. Currently
+          I'm working on my UX design dark side so that you can look forward on
+          seeing my Google Coursera UX Design Project/s here.
+        </p>
       </div>
     </>
   );
