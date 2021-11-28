12c9142fa3b7cbddce8aa353f853002bc0ac0cca
Jana Malecka <jm.jmalecka@gmail.com>
2021-11-28 14:23:49 +0100

    a added

--- src/pages/Work/index.jsx
+++ src/pages/Work/index.jsx
@@ -1,37 +1,48 @@
 import React from 'react';
 import './style.css';
-/* import hradejovSmallUrl from './img/hradejovSmallUrl.png'; */
+import hradejovSmallUrl from './img/hradejovSmall.png';
 
 const Work = () => {
   return (
     <>
       <div className="container">
-        <h2>Czechitas Web Academy</h2>
-        <h3>Final project</h3>
-        <div className="project-info">
-          <a href="https://github.com/JanaMalecka/Hradejov">
-            <p className="code">Code</p>
-          </a>
+        <div className="container__project">
+          <h2>Czechitas Web Academy</h2>
+          <h3>Final project</h3>
+          <div className="project-info">
+            <a href="https://github.com/JanaMalecka/Hradejov">
+              <p className="code">Code</p>
+            </a>
+            <a href="https://www.hradejov.cz/">
+              <p className="code">Website</p>
+            </a>
+          </div>
           <a href="https://www.hradejov.cz/">
-            <p className="code">Website</p>
+            <img
+              src={hradejovSmallUrl}
+              className="img-hradejov"
+              alt="Home page of the Hradejov website"
+            ></img>
           </a>
         </div>
-        {/* <img src={hradejovSmallUrl}></img> */}
-
-        <h2>ReactGirls - Academy for advanced</h2>
-        <h3>Workshop project</h3>
-        <div className="project-info">
-          <p className="code">Code</p>
-          <p className="code">Website</p>
+        <div className="container__project">
+          <h2>ReactGirls - Academy for advanced</h2>
+          <h3>Workshop project</h3>
+          <div className="project-info">
+            <p className="code">Code</p>
+            <p className="code">Website</p>
+          </div>
+          <img></img>
         </div>
-        <img></img>
-        <h2>Google UX Design - Coursera (professional certificate)</h2>
-        <h3>Case Study</h3>
-        <div className="project-info">
-          <p className="code">Code</p>
-          <p className="code">Website</p>
+        <div className="container__project">
+          <h2>Google UX Design - Coursera (professional certificate)</h2>
+          <h3>Case Study</h3>
+          <div className="project-info">
+            <p className="code">Code</p>
+            <p className="code">Website</p>
+          </div>
+          <img></img>
         </div>
-        <img></img>
       </div>
     </>
   );
